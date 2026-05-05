ca00ddf22dd4eb350d5a15a0f10ee8fc9b43a448
outamghartAchraf <outamghart2000@gmail.com>
2026-05-05 14:22:32 +0100

    add function addBook for create new book

--- src/Services/Library.php
+++ src/Services/Library.php
@@ -10,7 +10,32 @@ class Library
         $this->pdo = DB::connect();
     }
 
+    public function getAllBooks()
+    {
+        $sqlState = $this->pdo->query("SELECT * FROM books");
+        $books = $sqlState->fetchAll(PDO::FETCH_OBJ);
+
+        echo "\n===== BOOKS LIST =====\n";
+
+        foreach ($books as $book) {
+            echo "ID: {$book->id} | Title: {$book->title} | Author: {$book->author} | Status: {$book->status}\n";
+        }
+    }
+
+    public function addBook($title, $author, $isbn, $libraryId) {
+
+        $stmt = $this->pdo->prepare("
+            INSERT INTO books (title, author, isbn, status, library_id)
+            VALUES (?, ?, ?, 'available', ?)
+        ");
+
+        $stmt->execute([$title, $author, $isbn, $libraryId]);
+
+        echo "Book added successfully\n";
+    }
 
 
 }
 
+$library = new Library();
+ 
