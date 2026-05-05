96e42019c3bb99c37a446e0fafbb70ece4014125
outamghartAchraf <outamghart2000@gmail.com>
2026-05-05 10:46:20 +0100

    create class user

--- src/models/User.php
+++ src/models/User.php
@@ -2,9 +2,22 @@
 
 abstract class User {
 
-    protected $id;
     protected $name;
     protected $email;
+  
+    public function __construct($name, $email)
+    {
+      
+        $this->name = $name;
+        $this->email = $email;
+    }
 
- 
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
 }
