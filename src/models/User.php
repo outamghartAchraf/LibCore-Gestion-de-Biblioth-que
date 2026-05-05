b13f1affe53391565fb7311d761d5cc7eded3dd8
outamghartAchraf <outamghart2000@gmail.com>
2026-05-05 10:47:42 +0100

    add settesr in class user

--- src/models/User.php
+++ src/models/User.php
@@ -1,23 +1,35 @@
 <?php
 
-abstract class User {
+abstract class User
+{
 
     protected $name;
     protected $email;
-  
+
     public function __construct($name, $email)
     {
-      
+
         $this->name = $name;
         $this->email = $email;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
 
-    public function getEmail() {
+    public function getEmail()
+    {
         return $this->email;
     }
 
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
 }
