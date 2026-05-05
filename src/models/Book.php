d0266e8651a2898f49da8ec578388ef4abe0df0d
outamghartAchraf <outamghart2000@gmail.com>
2026-05-05 10:59:31 +0100

    add getters to class book

--- src/models/Book.php
+++ src/models/Book.php
@@ -1,10 +1,41 @@
-<?php 
+<?php
 
 class Book {
-    private $id;
+ 
     private $title;
     private $author;
     private $isbn;
     private $status;
     private $libraryId;
+
+    public function __construct( $title , $author , $isbn  , $status, $libraryId) {
+     
+        $this->title = $title;
+        $this->author = $author;
+        $this->isbn = $isbn;
+        $this->status = $status;
+        $this->libraryId = $libraryId;
+    }
+ 
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    public function getIsbn() {
+        return $this->isbn;
+    }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function getLibraryId() {
+        return $this->libraryId;
+    }
+
+ 
 }
