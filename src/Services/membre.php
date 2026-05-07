5c867cf266abb895ff7e8811bae5cbc68552dc84
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-07 11:17:18 +0100

    Afficher les livres empruntés

--- src/Services/membre.php
+++ src/Services/membre.php
@@ -7,4 +7,9 @@ echo " ==== DASHBOARD MEMBRE ====\n";
 
 while (true) {
     echo "\n 1-Afficher tout les livres \n";
+     echo "\n 1-Livre empruntés \n";
+      echo "\n 1-Afficher tout les livres \n";
+       echo "\n 1-Afficher tout les livres \n";
+
+    
 }
