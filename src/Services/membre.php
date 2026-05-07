9926e202a1da6e89323582e62ad7f24daa85d520
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-07 11:54:58 +0100

    Variable pour choisir une option

--- src/Services/membre.php
+++ src/Services/membre.php
@@ -1,5 +1,5 @@
 <?php
-require_once "src/Services/library.php";
+require_once __DIR__ . "/library.php";
 
 $library = new Library();
 
@@ -7,9 +7,12 @@ echo " ==== DASHBOARD MEMBRE ====\n";
 
 while (true) {
     echo "\n 1-Afficher tout les livres \n";
-     echo "\n 1-Livre empruntés \n";
-      echo "\n 1-Afficher tout les livres \n";
-       echo "\n 1-Afficher tout les livres \n";
+     echo "\n 2-emprunter un livre \n";
+      echo "\n 3-Retourner un livre \n";
+       echo "\n 0-Quitter \n";
+       $choix= readline("Choissisez une option");
+       
+       
 
     
 }
