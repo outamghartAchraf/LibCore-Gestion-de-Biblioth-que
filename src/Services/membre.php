9be2a4ae3fa638788fa5196f8c3d357009c8ab1b
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-08 09:48:47 +0100

    Ajouter un choix de recherche pour un membre

--- src/Services/membre.php
+++ src/Services/membre.php
@@ -9,6 +9,7 @@ while (true) {
     echo "\n 1-Afficher tout les livres \n";
      echo "\n 2-emprunter un livre \n";
       echo "\n 3-Retourner un livre \n";
+      echo "\n 4-Rechercher un livre \n";
        echo "\n 0-Quitter \n";
        $choix= readline("Choissisez une option");
        
