9d2cadc0032faa1ab95fc60f2a2990f5dbd48556
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-08 15:40:11 +0100

    separer logique et l'affichage

--- mainMember.php
+++ mainMember.php
@@ -1,3 +1,62 @@
 <?php
 
-require_once __DIR__ . "/services/membre.php";
+require_once __DIR__ . "/src/Services/Membre.php";
+
+$membre = new Membre();
+
+echo " ==== DASHBOARD MEMBRE ====\n";
+
+while (true) {
+
+    echo "\n1. Afficher tous les livres\n";
+    echo "2. Emprunter un livre\n";
+    echo "3. Retourner un livre\n";
+    echo "4. Rechercher un livre\n";
+    echo "0. Quitter\n";
+
+    $choix = readline("Choisissez une option : ");
+
+    switch ($choix) {
+
+        case 1:
+            $membre->afficherLivres();
+            break;
+
+        case 2:
+            $memberID = readline("ID membre : ");
+            $bookID = readline("ID livre : ");
+
+            $membre->emprunterLivre($memberID, $bookID);
+            break;
+
+        case 3:
+            $loanID = readline("ID emprunt : ");
+            $bookID = readline("ID livre : ");
+
+            $membre->retournerLivre($loanID, $bookID);
+            break;
+
+        case 4:
+            $input = readline("Titre ou auteur : ");
+
+            $resultats = $membre->rechercherLivre($input);
+
+            if (empty($resultats)) {
+                echo "Aucun livre trouvé\n";
+            } else {
+                foreach ($resultats as $book) {
+                    echo "ID: {$book->id} | ";
+                    echo "Titre: {$book->title} | ";
+                    echo "Auteur: {$book->author} | ";
+                    echo "Status: {$book->status}\n";
+                }
+            }
+            break;
+
+        case 0:
+            exit("Au revoir !\n");
+
+        default:
+            echo "Option invalide\n";
+    }
+}
--- src/Services/membre.php
+++ src/Services/membre.php
@@ -1,72 +1,46 @@
 <?php
-require_once __DIR__ . "/library.php";
+require_once __DIR__ . "/Library.php";
 
-$library = new Library();
+class Membre
+{
+    private $library;
 
-echo " ==== DASHBOARD MEMBRE ====\n";
-
-while (true) {
-    echo "\n 1-Afficher tout les livres \n";
-     echo "\n 2-emprunter un livre \n";
-      echo "\n 3-Retourner un livre \n";
-      echo "\n 4-Rechercher un livre \n";
-       echo "\n 0-Quitter \n";
-       $choix= readline("Choissisez une option");
-       
-    switch ($choix){
-        case 1:
-            $library->getAllBooks();
-            break;
-
-            case 2:
-                $membreID=readline("Entrez Membre identifiant:");
-                $bookID=readline("Entrez l'identifiant du livre:");
-
-                $library->borrowBook($membreID,$bookID);
-                break;
-            case 3:
-                $emprunter=readline("Entrez Id emprunté");
-                $bookID=readline("Entrez Id book");
-    $library->returnBook($emprunter, $bookID);
-            break;
-
-
-            case 4:
-
-                $input=readline("Entrez le titre ou l'auteur:");
-                $books=$library->getAllBooks();
-                $trouveLivre= false;
-
-                foreach ($books as $book){
-                 
-                if( stripos($book['title'],$input) !== false || stripos($book['author'],$input) !==false  ){
-
-
-            echo "ID: {$book['id']} | ";
-            echo "Titre: {$book['title']} | ";
-            echo "Auteur: {$book['author']} | ";
-            echo "Status: {$book['status']}\n";
-             
- 
-                        $trouveLivre= true;
-                }
-                 
-
-                }
-
-                if (!$trouveLivre) {
-        echo "Aucun livre trouvé.\n";
+    public function __construct()
+    {
+        $this->library = new Library();
     }
 
-    break;
-
-            case 0:
-                exit("Au revoir ! merci pour votre visite");
-
-          default: echo "L'option n'est pas valide\n";      
-
+    public function afficherLivres()
+    {
+        return $this->library->getAllBooks();
     }
 
+    public function emprunterLivre($memberId, $bookId)
+    {
+        $this->library->borrowBook($memberId, $bookId);
+    }
 
-    
+    public function retournerLivre($loanId, $bookId)
+    {
+        $this->library->returnBook($loanId, $bookId);
+    }
+
+    public function rechercherLivre($input)
+    {
+        $books = $this->library->getAllBooks();
+
+        $resultats = [];
+
+        foreach ($books as $book) {
+
+            if (
+                stripos($book->title, $input) !== false ||
+                stripos($book->author, $input) !== false
+            ) {
+                $resultats[] = $book;
+            }
+        }
+
+        return $resultats;
+    }
 }
