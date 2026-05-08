d84647fbc99d74f08d2df4433a6b8bbc62f527fa
outamghartAchraf <126915672+outamghartAchraf@users.noreply.github.com>
2026-05-08 16:23:16 +0100

    Update mainAdmin.php

--- mainAdmin.php
+++ mainAdmin.php
@@ -2,40 +2,78 @@
 
 require_once __DIR__ . "/src/Services/Library.php";
 
+class MainAdmin
+{
+    private $library;
 
-$library = new Library();
-while (true) {
+    public function __construct()
+    {
+        $this->library = new Library();
+    }
 
-    echo "\n===================================\n";
-    echo "   LIBRARIAN DASHBOARD\n";
-    echo "=====================================\n";
+    public function run()
+    {
+        while (true) {
 
-    echo "1. Show Books (Stock)\n";
-    echo "2. Add Book\n";
-    echo "3. Create Member\n";
-    echo "4. Borrow Book\n";
-    echo "5. Return Book\n";
-    echo "6. Delete Book\n";
-    echo "7. Exit\n";
+            echo "\n===================================\n";
+            echo "      LIBRARIAN DASHBOARD\n";
+            echo "===================================\n";
 
-    echo "Choose option: ";
-    flush();
-    $choice = readline();
+            echo "1. Show Books (Stock)\n";
+            echo "2. Add Book\n";
+            echo "3. Create Member\n";
+            echo "4. Borrow Book\n";
+            echo "5. Return Book\n";
+            echo "6. Delete Book\n";
+            echo "7. Mark Book as Under Repair\n";
+            echo "0. Exit\n";
 
-    switch ($choice) {
-        case 1 :
-            $library->getAllBooks();
-            break;
+            echo "\nChoose option: ";
 
-        case 2 :
-            echo "Enter Title: ";
-            flush();
-            $title = readline();
+            $choice = trim(readline());
 
-            echo "Enter Author: ";
-            flush();
-            $author = readline();
+            switch ($choice) {
 
+                case 1:
+                    $this->library->getAllBooks();
+                    break;
 
+                case 2:
+                    $this->library->addBook();
+                    break;
+
+                case 3:
+                    $this->library->addMember();
+                    break;
+
+                case 4:
+                    $this->library->getAllBooks();
+                    $this->library->borrowBook();
+                    break;
+
+                case 5:
+                    $this->library->returnBook();
+                    break;
+
+                case 6:
+                    $this->library->getAllBooks();
+                    $this->library->deleteBook();
+                    break;
+
+                case 7:
+                    $this->library->getAllBooks();
+                    $this->library->markAsRepair();
+                    break;
+
+                case 0:
+                    echo "Good Bye \n";
+                    exit;
+
+                default:
+                    echo "Invalid operation \n";
+            }
+        }
     }
 }
+
+
